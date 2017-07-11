7d4ef2b2feffa3b4104492b4219eb3b1b5f6df9d
farahpoor <a.farahpoor@gmail.com>
2017-07-11 15:17:27 +0430

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$host="localhost"; // Host name
+$username="*****"; // Mysql username
+$password="*****"; // Mysql password
+$db_name="mydatabase"; // Database name
+
+$web_path="./public_html/" //address of your files on the server(such as "/public_html/").
+$from="info@example.com " //email address of your website (such as: info@example.com).
+$to= "me@gmail.com" //email of yours (such as : me@gmail.com).
+$subject="The Webi files have been Threatened"  //subject of email
+
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $db_name);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
