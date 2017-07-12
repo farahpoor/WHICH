7e7b7a13c7c8b94b170b10c79c72275853c3f271
farahpoor <a.farahpoor@gmail.com>
2017-07-12 18:26:52 +0430

    Update and rename check_md5.php to check_sha512.php

--- check_sha512.php
+++ check_sha512.php
@@ -8,8 +8,6 @@
 	$nf=0;  //nt means not found
 	$nm=0;  //nm means not match
 	$np=0;  //no problem(OK)
-
-
 	function send_email($from,$to,$getSubject,$getmessage)
     {
 		$headers  = 'MIME-Version: 1.0' . "\r\n";
@@ -54,29 +52,27 @@
 	} 
 	
 	$result_array=getDirContents($web_path, '/\.php$/');
-
 	
-	echo "<table border='1'><tr><td>Row</td><td>File Name</td><td>Path</td><td>MD5 Hash</td><td>Status</td></tr>";
+	echo "<table border='1'><tr><td>Row</td><td>File Name</td><td>Path</td><td>sha512 Hash</td><td>Status</td></tr>";
 	foreach ($result_array as $path) {
 		$filename=basename($path);
-		$md5_hash=md5_file($path,false);		
-		echo "<tr><td>" . $row_count . "</td><td>" . $filename . "</td><td>".$path ."</td><td>" . $md5_hash . "</td>";
+		$sha512_hash=hash('sha512', $path);		
+		echo "<tr><td>" . $row_count . "</td><td>" . $filename . "</td><td>".$path ."</td><td>" . $sha512_hash . "</td>";
 		
-		$sql = "SELECT * FROM file_md5 WHERE Source_File_Path='$path'";		
+		$sql = "SELECT * FROM file_sha512 WHERE Source_File_Path='$path'";		
 		$result = $conn->query($sql);
 		
 		if ($result->num_rows > 0) 
 		{
 			while($row = $result->fetch_assoc()) 
 			{
-				if($row['MD5']==$md5_hash) {echo "<td><div style='color: #00cc66'>OK</div></td>"; $np++;}
-				else {echo "<td><div style='color: red'>MD5 not matches!</div></td>"; $nm++; $problem_array[$problem_counter][0]=$path;$problem_array[$problem_counter][1]="MD5 Not Match";$problem_counter++;$flag=true;}
+				if($row['sha512']==$sha512_hash) {echo "<td><div style='color: #00cc66'>OK</div></td>"; $np++;}
+				else {echo "<td><div style='color: red'>sha512 not matches!</div></td>"; $nm++; $problem_array[$problem_counter][0]=$path;$problem_array[$problem_counter][1]="sha512 Not Match";$problem_counter++;$flag=true;}
 			}
 		}
 		else{ echo "<td><div style='color: #ff9900'>File Not found in the Database!</div></td></tr>"; $nf++; $problem_array[$problem_counter][0]=$path;$problem_array[$problem_counter][1]="File not found";$problem_counter++;$flag=true;}
 		$row_count++;
 	}
-
 	echo "</table>";
 	echo "<center><table style='width:50%'><tr><td>OK</td><td>Not match</td><td>Not found</td><tr>";
 	echo "<tr><td>".$np."</td><td>".$nm."</td><td>".$nf."</td></tr>";
