5b41edb1953abb3a07276a4eedbbcd77bd09fdbd
farahpoor <a.farahpoor@gmail.com>
2022-12-05 14:54:28 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ $username="*****"; // Mysql username
 $password="*****"; // Mysql password
 $db_name="mydatabase"; // Database name
 
-$web_path="/public_html/" //address of your files on the server(such as "/public_html/").
-$from="info@example.com " //email address of your website (such as: info@example.com).
-$to= "me@gmail.com" //email of yours (such as : me@gmail.com).
-$subject="The Web files have been Threatened"  //subject of email
+$web_path="/public_html/"; //address of your files on the server(such as "/public_html/").
+$from="info@example.com"; //email address of your website (such as: info@example.com).
+$to= "me@gmail.com"; //email of yours (such as : me@gmail.com).
+$subject="The Web files have been Threatened";  //subject of email
 
 
 // Create connection
